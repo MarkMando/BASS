2823676254ec6110131cc7f6a2a3882afe596f9c
mmando <mark.mando@fqml.com>
2026-06-18 15:25:58 +0200

    Updated the ultrasound_mobilenetv3_int8.tflite file

--- app/src/main/java/com/mando/bascanner/Modality.kt
+++ app/src/main/java/com/mando/bascanner/Modality.kt
@@ -1,7 +1,7 @@
 package com.mando.bascanner
 
 enum class Modality(val title: String, val modelFile: String, val inputSize: Int) {
-    ULTRASOUND("Ultrasound Analysis", "busi_float32.tflite", 224),
-    MAMMOGRAPHY("Mammography Analysis", "mammo_native_float32.tflite", 224),
+    ULTRASOUND("Ultrasound Analysis", "ultrasound_mobilenetv3_int8", 224),
+    MAMMOGRAPHY("Mammography Analysis", "mammo_mobilenetv3_int8.tflite", 224),
     EXTERIOR("Exterior Examination", "ext_mobilenetv3_int8.tflite", 224)
 }
